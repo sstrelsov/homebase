7ee2033accbf6959e1d340233b37cf0fffbe1b8d
Spencer Strelsov <sstrelsov@gmail.com>
2024-12-21 13:07:06 -0500

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./css/index.css";
 import BioPage from "./pages/Bio";
 import LandingPage from "./pages/Landing";
 import ProjectsPage from "./pages/Projects";
+
 const App = () => {
   return (
     <div className="h-screen flex flex-col">
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,23 @@
 import { Image } from "@nextui-org/react";
+import { useTheme } from "@nextui-org/use-theme";
 import Typewriter from "../components/Typewriter";
 const LandingPage = () => {
+  const { theme, setTheme } = useTheme();
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <div className="flex flex-grow flex-col w-full h-full items-center justify-center gap-4">
       <Image
+        className="hover:cursor-pointer"
         src="/images/strelsov-headshot.png"
         alt="Spencer Strelsov Headshot"
         isBlurred
         isZoomed
         width={240}
+        onClick={handleThemeSwitch}
       />
       <Typewriter
         phrases={[
