2346a6233bf5a171e672fb1960097b4bbeab5d09
Spencer Strelsov <sstrelsov@gmail.com>
2024-12-22 12:20:39 -0500

    gets rid of bleed on image

--- src/pages/Bio.tsx
+++ src/pages/Bio.tsx
@@ -1,6 +1,6 @@
 const BioPage = () => {
   return (
-    <div className="flex flex-col gap-4 max-w-sm sm:max-w-lg">
+    <div className="flex flex-col gap-4 max-w-sm sm:max-w-lg h-full justify-center">
       <p className="text-lg">
         I'm passionate about the intersection of the humanities and technology.
         I have a background in history and computer science, and I'm interested
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,6 @@
 const ProjectsPage = () => {
   return (
-    <div>
+    <div className="flex flex-col max-w-sm sm:max-w-lg h-full justify-center">
       <h1 className="text-3xl">Coming Sooooon</h1>
     </div>
   );
