903c65197479177f13744a34b539e11c6d1502c5
Spencer Strelsov <sstrelsov@gmail.com>
2024-12-23 18:40:02 -0500

    homebase links are dynamic now

--- src/components/HomebaseLink.tsx
+++ src/components/HomebaseLink.tsx
@@ -1,18 +1,40 @@
-import { Link, LinkProps } from "@nextui-org/link";
+import { AnchorHTMLAttributes } from "react";
 import { useLinkColor } from "../utils/ColorContext";
 
-const HomebaseLink = (props: LinkProps) => {
+interface HomebaseLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  /** Tailwind text classes like "text-lg", "text-sm", etc. */
+  size?: string;
+}
+
+/**
+ * A normal <a> link with:
+ *  - text color inherited from parent
+ *  - underline color from linkColor
+ *  - optional size prop (e.g. "text-lg")
+ */
+export default function HomebaseLink({
+  size,
+  className = "",
+  style,
+  children,
+  ...props
+}: HomebaseLinkProps) {
   const { linkColor } = useLinkColor();
 
   return (
-    <Link
-      className="text-inherit"
-      style={{ textDecorationColor: linkColor }}
-      underline="always"
-      size="lg"
-      {...(props as any)}
-    />
+    <a
+      // Merge any custom classes with "text-inherit underline" + size
+      className={`text-inherit underline ${className}`}
+      // Merge any inline styles with textDecorationColor
+      style={{
+        textDecorationColor: linkColor,
+        textDecorationLine: "underline",
+        textUnderlineOffset: "0.15em",
+        ...style,
+      }}
+      {...props}
+    >
+      {children}
+    </a>
   );
-};
-
-export default HomebaseLink;
+}
