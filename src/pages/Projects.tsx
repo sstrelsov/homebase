6122189271b1c327aa227e98142f9dbd22e04c2b
Spencer Strelsov <sstrelsov@gmail.com>
2024-12-25 15:18:08 -0500

    remove padding

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const ProjectsPage = () => {
   return (
-    <div className="flex flex-col w-full h-full justify-center text-center gap-2 px-6">
+    <div className="flex flex-col w-full h-full justify-center text-center gap-2">
       <Outlet />
     </div>
   );
