13b34bc68c386b53e3213bdd5cad96caf0b66642
Spencer Strelsov <sstrelsov@gmail.com>
2024-12-30 14:04:09 -0500

    side scroll on buttons if too many on mobile

--- src/projects/globe-project/CountryButtons.tsx
+++ src/projects/globe-project/CountryButtons.tsx
@@ -1,16 +1,34 @@
 import { Button } from "@nextui-org/react";
 import Flag from "react-flagpack";
+import useAtOrAboveBreakpoint from "../../utils/useAtOrAboveBreakpoint";
 
+const MAX_BUTTONS_VISIBLE_MOBILE = 5;
+const MAX_BUTTONS_VISIBLE_DESKTOP = 10;
+const BUTTON_WIDTH_PX = 64;
 interface CountryButtonProps {
-  isos: string[] /** Must be alpha-3 */;
+  isos: string[]; // Must be alpha-3
   onClick?: (iso: string) => void;
 }
+
 const CountryButtons = ({ isos, onClick }: CountryButtonProps) => {
   const handleClick = (iso: string) => {
     onClick?.(iso);
   };
+
+  // Use a breakpoint to decide how many buttons we display before scroll
+  const isSmUp = useAtOrAboveBreakpoint("sm");
+  const maxButtonCount = isSmUp
+    ? MAX_BUTTONS_VISIBLE_DESKTOP
+    : MAX_BUTTONS_VISIBLE_MOBILE;
+
+  const maxWidth = maxButtonCount * BUTTON_WIDTH_PX;
+
   return (
-    <>
+    <div
+      className="flex flex-row gap-2 overflow-x-auto [mask-image:linear-gradient(to_right,transparent,black_5%,black_95%,transparent)]
+"
+      style={{ maxWidth }}
+    >
       {isos.map((iso) => (
         <Button
           size="md"
@@ -22,7 +40,7 @@ const CountryButtons = ({ isos, onClick }: CountryButtonProps) => {
           <Flag code={iso} hasBorder={false} size="l" />
         </Button>
       ))}
-    </>
+    </div>
   );
 };
 
