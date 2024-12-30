cdcfa80c230e46a8484d274ec94b29cc29f5367c
Spencer Strelsov <sstrelsov@gmail.com>
2024-12-30 11:37:29 -0500

    new earth and atmosphere colors

--- src/components/earth/EarthScene.tsx
+++ src/components/earth/EarthScene.tsx
@@ -17,7 +17,6 @@ import CityMarkers from "./layers/CityMarkerGroup";
 import LandDots from "./layers/LandDots";
 import SceneHelpers from "./SceneHelpers";
 import { useLandDotsData } from "./useLandDotsData";
-
 const MAX_ZOOMED_OUT = 600;
 export const EARTH_RADIUS = 150;
 
@@ -49,7 +48,7 @@ const EarthScene = ({
   const isSmallUp = useAtOrAboveBreakpoint("sm");
 
   const { isLoading, dots } = useLandDotsData(
-    `/landDots-150rad-${isSmallUp ? "60" : "30"}k.json`
+    `/landDots-150rad-${isSmallUp ? "60" : "25"}k.json`
   );
   console.log(spotlightCities);
   return (
@@ -68,8 +67,8 @@ const EarthScene = ({
           enableRotate={false}
           enableDamping={true}
           minDistance={300}
-          minPolarAngle={0.3} // ~17 degrees
-          maxPolarAngle={Math.PI - 0.3} // ~163 degrees
+          minPolarAngle={0.3}
+          maxPolarAngle={Math.PI - 0.3}
           enablePan={false}
           maxDistance={MAX_ZOOMED_OUT}
         />
@@ -77,14 +76,14 @@ const EarthScene = ({
         <directionalLight
           intensity={1.6}
           color="#b3e2ff" // bright, cool blue
-          position={[300, 300, 300]}
+          position={[300, 3000, 300]}
         />
 
         {/* Fill Light: from left-ish side, weaker */}
         <directionalLight
           intensity={2}
           color="#df3dff"
-          position={[-300, -100, -500]}
+          position={[-3000, -100, -300]}
         />
 
         <directionalLight
@@ -95,9 +94,9 @@ const EarthScene = ({
 
         {/* Rim Light: behind the globe for silhouette & drama: Creates a beautiful purple in top right */}
         <directionalLight
-          intensity={1.8}
+          intensity={3.8}
           color="#ff7a3d"
-          position={[300, 100, -100]} // -300 also beautiful
+          position={[3000, 100, -600]} // -300 also beautiful
         />
 
         {/* Kicker Light: from below or another interesting angle */}
@@ -105,6 +104,11 @@ const EarthScene = ({
           intensity={1.4}
           color="#8066ff"
           position={[-200, -200, -100]} // z=-300 also beautiful
+        />
+        <directionalLight
+          intensity={1.4}
+          color="#ff9166"
+          position={[-800, 0, -800]} // z=-300 also beautiful
         />
 
         <hemisphereLight intensity={0.7} position={[100, 100, 0]} />
@@ -121,24 +125,26 @@ const EarthScene = ({
                 <BaseSphere
                   radius={EARTH_RADIUS - 1}
                   color="#533f7b"
-                  emissive="#24083c"
+                  emissive="#08020e"
                   emissiveIntensity={0.4}
                   shininess={5}
                   specular="#222222"
                 />
                 <LandDots
-                  dotColor="#df8cfd"
-                  highlightColor="#b908f9"
+                  dotColor="#d9c5f4"
+                  highlightColor="#edb0ff"
                   dots={dots}
-                  pointSize={2.5}
+                  pointSize={isSmallUp ? 2.7 : 3}
                   spotlightCountries={spotlightCountries}
+                  edgeFadeStart={isSmallUp ? 0.6 : 0.7}
+                  edgeFadeEnd={isSmallUp ? 0.3 : 0.25}
                 />
                 <Atmosphere
                   radius={EARTH_RADIUS}
-                  scaleFactor={1.001}
-                  color="#f4bcf6"
-                  power={5.0}
-                  intensity={1.5}
+                  scaleFactor={1}
+                  color="#bc95f3"
+                  power={15}
+                  intensity={5}
                   opacity={0.5}
                 />
                 {!spotlightCountries && !spotlightCities && (
@@ -159,7 +165,7 @@ const EarthScene = ({
                   <CityMarkers
                     cities={spotlightCities}
                     radius={EARTH_RADIUS}
-                    color="#dd6ff0"
+                    color="#e6b3ff"
                     markerSize={1}
                   />
                 )}
