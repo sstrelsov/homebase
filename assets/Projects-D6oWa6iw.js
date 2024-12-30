43a0a29bb2f2db9d2acd9e154944f222a7fc6cda
Spencer Strelsov <sstrelsov@gmail.com>
2024-12-30 15:11:36 -0500

    Force deploy from branch main

--- assets/Projects-D6oWa6iw.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as t}from"./index-DabGiqIS.js";const l=()=>e.jsx("div",{className:"flex flex-col w-full h-full justify-center text-center gap-2",children:e.jsx(t,{})});export{l as default};
---
+++ assets/useAtOrAboveBreakpoint-p9HrBNde.js
@@ -0,0 +1,1 @@
+import{r}from"./index-D9fWAZR-.js";const a={xs:320,sm:640,md:768,lg:1024,xl:1280,"2xl":1536},u=t=>{const[i,o]=r.useState(!1);return r.useEffect(()=>{function e(){const s=window.innerWidth>=a[t];o(n=>n!==s?s:n)}return window.addEventListener("resize",e),e(),()=>window.removeEventListener("resize",e)},[t]),i};export{u};
