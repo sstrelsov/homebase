43ac4acc4746eb00d7b7e7464318880ca0d8466a
Spencer Strelsov <sstrelsov@gmail.com>
2025-01-01 20:01:51 -0500

    Force deploy from branch main

---
+++ assets/Projects-D0MxQMgz.js
@@ -0,0 +1,1 @@
+import{j as e,O as t}from"./index-Uz8SC1VW.js";const l=()=>e.jsx("div",{className:"flex flex-col w-full h-full justify-center text-center gap-2",children:e.jsx(t,{})});export{l as default};
